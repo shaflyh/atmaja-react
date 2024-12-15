ecedacd68c0057e6072f320ef6c0285f298a4870
Shafly Hamzah <shafly2000@gmail.com>
2024-12-15 20:18:33 +0700

    feat: update under construction design

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,26 +10,42 @@ function MyApp({ Component, pageProps }) {
     const underConstruction =
         process.env.NEXT_PUBLIC_UNDER_CONSTRUCTION === 'true';
 
-    if (underConstruction && router.pathname !== '/under-construction') {
+    if (underConstruction) {
         // Redirect all routes to the "Under Construction" page
         return (
             <>
                 <Head>
-                    <title>Under Construction</title>
+                    <title>Atmaja Global Edu - Under Construction</title>
                     <meta
                         name="description"
                         content="This page is under construction. Please check back later."
                     />
                 </Head>
-                <div className="flex items-center justify-center min-h-screen bg-gray-100">
-                    <div className="text-center">
-                        <h1 className="text-4xl font-bold text-gray-800">
-                            🚧 Under Construction 🚧
+                <div className="flex items-center justify-center min-h-screen text-white bg-black">
+                    <div className="px-4 text-center sm:px-8">
+                        <h1 className="text-3xl font-bold tracking-wide sm:text-4xl md:text-5xl">
+                            🚧 Under Construction
                         </h1>
-                        <p className="mt-4 text-lg text-gray-600">
-                            We&aposre working hard to bring something amazing.
-                            Please check back soon!
+                        <p className="mt-6 text-base sm:text-lg">
+                            We are building something amazing for you. Stay
+                            tuned for updates!
                         </p>
+                        <div className="mt-8">
+                            <div
+                                className="w-12 h-1 mx-auto mb-4 border border-white sm:w-16 md:w-20"
+                                aria-hidden="true"
+                            />
+                            <p className="text-sm tracking-wider">
+                                <strong>Contact Us:</strong> info@atmajapin.com
+                            </p>
+                            {/* <p className="text-sm tracking-wider">
+                                <strong>Visit Again:</strong> 01/01/2025
+                            </p> */}
+                        </div>
+                        <footer className="mt-8 text-xs">
+                            © {new Date().getFullYear()} Atmaja Global Edu. All
+                            Rights Reserved.
+                        </footer>
                     </div>
                 </div>
             </>
